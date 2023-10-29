05d2a3f8b7299c87dc62f6daf9229ebaa7010644
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2023-10-29 17:36:40 +0100

    clean deployment

--- deployment/deployment.tf
+++ deployment/deployment.tf
@@ -18,7 +18,7 @@ resource "kubernetes_service" "microservice1_service" {
       port       = 8080
     }
 
-    type = "LoadBalancer"
+    type = "ClusterIP"
   }
 }
 
@@ -56,65 +56,6 @@ resource "kubernetes_deployment" "microservice1_deployment" {
           port {
             container_port  = 8080
           }
-        }
-      }
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress-controller"
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "controller.ingressClass"
-    value = "nginx"
-  }
-}
-
-resource "kubernetes_ingress_v1" "main_ingress" {
-  metadata {
-    name = "main-ingress"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
-
-  spec {
-    default_backend {
-      service {
-        name = "microservice1-service"
-        port {
-          number = 8080
-        }
-      }
-    }
-
-    rule {
-      http {
-        path {
-          backend {
-            service {
-              name = "microservice1-service"
-              port {
-                number = 8080
-              }
-            }
-          }
-
-          path = "/"
         }
       }
     }
